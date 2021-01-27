f160343aee0d5d51791256dcbc128b861f41f200
Will Papp <wpapp90@gmail.com>
2021-01-26 19:05:24 -0600

    all slot texts are now saved even after page refresh

--- develop/js/script.js
+++ develop/js/script.js
@@ -17,6 +17,63 @@ $(document).ready(function () {
     });
     $("#textarea9am").val(localStorage.getItem("09:00 AM"));
 
+    $("#saveBtn10am").on("click", function () {
+        var textArea10am = $("#textarea10am").val();
+        var hour10 = $("#hour10").text();
+        localStorage.setItem(hour10, textArea10am);
+    });
+    $("#textarea10am").val(localStorage.getItem("10:00 AM"));
+
+    $("#saveBtn11am").on("click", function () {
+        var textArea11am = $("#textarea11am").val();
+        var hour11 = $("#hour11").text();
+        localStorage.setItem(hour11, textArea11am);
+    });
+    $("#textarea11am").val(localStorage.getItem("11:00 AM"));
+
+    $("#saveBtn12pm").on("click", function () {
+        var textArea12pm = $("#textarea12pm").val();
+        var hour12 = $("#hour12").text();
+        localStorage.setItem(hour12, textArea12pm);
+    });
+    $("#textarea12pm").val(localStorage.getItem("12:00 PM"));
+    
+    $("#saveBtn1pm").on("click", function () {
+        var textArea1pm = $("#textarea1pm").val();
+        var hour1 = $("#hour1").text();
+        localStorage.setItem(hour1, textArea1pm);
+    });
+    $("#textarea1pm").val(localStorage.getItem("01:00 PM"));
+
+    $("#saveBtn2pm").on("click", function () {
+        var textArea2pm = $("#textarea2pm").val();
+        var hour2 = $("#hour2").text();
+        localStorage.setItem(hour2, textArea2pm);
+    });
+    $("#textarea2pm").val(localStorage.getItem("02:00 PM"));
+
+    $("#saveBtn3pm").on("click", function () {
+        var textArea3pm = $("#textarea3pm").val();
+        var hour3 = $("#hour3").text();
+        localStorage.setItem(hour3, textArea3pm);
+    });
+    $("#textarea3pm").val(localStorage.getItem("03:00 PM"));
+
+    $("#saveBtn4pm").on("click", function () {
+        var textArea4pm = $("#textarea4pm").val();
+        var hour4 = $("#hour4").text();
+        localStorage.setItem(hour4, textArea4pm);
+    });
+    $("#textarea4pm").val(localStorage.getItem("04:00 PM"));
+
+    $("#saveBtn5pm").on("click", function () {
+        var textArea5pm = $("#textarea5pm").val();
+        var hour5 = $("#hour5").text();
+        localStorage.setItem(hour5, textArea5pm);
+    });
+    $("#textarea5pm").val(localStorage.getItem("05:00 PM"));
+
+
 });
 
 
